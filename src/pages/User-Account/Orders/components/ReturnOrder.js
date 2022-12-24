b91f72e8bf5160772c8aaf3e13b93c25bf9d4789
Craig Charlton <craigcharltonlunga1000@gmail.com>
2022-12-24 03:34:59 -0800

    added components to orders tab navigation  and some minor fixes(added icons and styling to the tab and tables)

--- src/pages/User-Account/Orders/components/ReturnOrder.js
+++ src/pages/User-Account/Orders/components/ReturnOrder.js
@@ -1,9 +1,91 @@
 import React from 'react'
+import {
+  Col,
+  Container,
+  Row,
+  Card,
+  CardBody,
+  CardTitle,
+  Button,
+  Badge,
+  CardText,
+  Collapse,
+  Nav,
+  NavItem,
+  NavLink,
+  TabContent,
+  TabPane,
+} from "reactstrap"
+import { orderDetails } from "./data"
+import { withRouter, Link } from "react-router-dom"
 
-function ReturnOrder() {
+function ReturnOrders() {
+
     return (
-        <h1>ReturnOrder</h1>
+        <Card>
+            <CardBody>
+              <CardTitle className="mb-4">Return Orders</CardTitle>
+              <div className="table-responsive">
+                <table className="table align-middle table-nowrap mb-0">
+                  <thead className="table-light">
+                    <tr>
+                      <th className="align-middle">Order ID</th>
+                      <th className="align-middle">Date</th>
+                      <th className="align-middle">Product image</th>
+                      <th className="align-middle">Product Details</th>
+                      <th className="align-middle">Price</th>
+                      <th className="align-middle"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {orderDetails.map((orderDetails, key) => (
+                      <tr key={"_tr_" + key}>
+                        <td>
+                          <Link to="#" className="text-body fw-bold">
+                            {" "}
+                            {orderDetails.orderId}{" "}
+                          </Link>{" "}
+                        </td>
+
+                      
+                        {/* Date */}
+                        <td>{orderDetails.Date}</td>
+
+                        {/* product image */}
+                        <td>
+                          <img className="rounded-circle"
+                            src="https://images.pexels.com/photos/14018171/pexels-photo-14018171.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
+                            style={{
+                              height: 50,
+                              width: 50,
+                            }}
+                          />
+                        </td>
+
+                        {/* product info */}
+                        <td>{orderDetails.productInfo}</td>
+
+                        {/* product price */}
+                        <td>{orderDetails.price}</td>
+
+                        <td className="align-end">
+                          <Button
+                            type="button"
+                            color="primary"
+                            size="sm"
+                            className="btn-rounded waves-effect waves-light"
+                          >
+                            Remove
+                          </Button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </CardBody>
+          </Card>
     )
 }
 
-export default ReturnOrder
+export default ReturnOrders
---
+++ src/pages/User-Account/Orders/components/data.js
@@ -0,0 +1,61 @@
+export const orderDetails = [
+    {
+      orderId: "#SK2540",
+      Date: "07 Oct, 2019",
+      productName:
+        "https://images.pexels.com/photos/14018171/pexels-photo-14018171.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+      productInfo: "MacBook air M2",
+      price: "$400",
+      link: "#",
+    },
+    {
+      id: "customCheck3",
+      orderId: "#SK2541",
+      Date: "07 Oct, 2019",
+      productName:
+        "https://images.pexels.com/photos/14018171/pexels-photo-14018171.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+      productInfo: "MacBook air M2",
+      price: "$380",
+      link: "#",
+    },
+    {
+      id: "customCheck4",
+      orderId: "#SK2542",
+      Date: "07 Oct, 2019",
+      productName:
+        "https://images.pexels.com/photos/14018171/pexels-photo-14018171.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+      productInfo: "MacBook air M2",
+      price: "$384",
+      link: "#",
+    },
+    {
+      id: "customCheck5",
+      orderId: "#SK2543",
+      Date: "07 Oct, 2019",
+      productName:
+        "https://images.pexels.com/photos/14018171/pexels-photo-14018171.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+      productInfo: "MacBook air M2",
+      price: "$412",
+      link: "#",
+    },
+    {
+      id: "customCheck6",
+      orderId: "#SK2544",
+      Date: "07 Oct, 2019",
+      productName:
+        "https://images.pexels.com/photos/14018171/pexels-photo-14018171.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+      productInfo: "MacBook air M2",
+      price: "$404",
+      link: "#",
+    },
+    {
+      id: "customCheck7",
+      orderId: "#SK2545",
+      Date: "07 Oct, 2019",
+      productName:
+        "https://images.pexels.com/photos/14018171/pexels-photo-14018171.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load",
+      productInfo: "MacBook air M2",
+      price: "$392",
+      link: "#",
+    },
+  ]
