149f26971192654fdebd28e4fd5879fab0860ff0
kito078 <101482292+kito078@users.noreply.github.com>
2022-12-25 22:52:42 +0200

    second commit

--- src/pages/User-Account/Profile-info/components/AddressDetails.js
+++ src/pages/User-Account/Profile-info/components/AddressDetails.js
@@ -1,9 +1,139 @@
 import React from 'react'
+import { AvForm, AvField } from "availity-reactstrap-validation"
+import { Button, Row, Col, Card, CardBody } from "reactstrap"
+import "../style.scss";
+import { BsHouseDoor } from 'react-icons/bs';
+import { MdOutlinePlace } from 'react-icons/md';
+import { FaStreetView } from 'react-icons/fa';
+import { RiBuildingLine } from 'react-icons/ri';
+import { MdLocationCity } from 'react-icons/md';
 
-function AddressDetails() {
+function PasswordAccount() {
     return (
-        <h1>AddressDetails</h1>
+        <Card className='card-container card-container-3'>
+            <CardBody className='body-wrapper-2'>
+                <AvForm
+                        className="form-horizontal mt-4"
+                        onValidSubmit={(e, v) => {
+                            handleValidSubmit(e, v)
+                        }}
+                        >
+                        
+                        <Row className='align-items-center justify-content-center'>
+                            <Col lg>
+                                <div className="mb-3 myInput">
+                                    <fieldset className="secInput-2 color-1">
+                                            <input className='' type='checkbox' name="checkbox"   />
+                                            <label >Business</label>
+                            
+                                    </fieldset>
+                                </div>
+                            </Col>
+                            <Col lg>
+                                <div className="mb-3 myInput">
+                                    <fieldset className="secInput-2 color-1">
+                                            <input className='' type='checkbox' name="checkbox" />
+                                            <label >Residential</label>
+                            
+                                    </fieldset>
+                                </div>
+                            </Col>
+                        </Row> 
+                        <Row className='align-items-center justify-content-center' >
+                            <Col lg>
+                                
+                                <div className="mb-3 myInput wrap-input">
+                                    <span className="icon"><MdOutlinePlace /></span>
+                                    <AvField 
+                                
+                                    name="province"
+                                    label="Province"
+                                    className="form-control first-form"
+                                    placeholder="Enter province"
+                                    type="text"
+                                    required
+                                    />
+                                </div>
+                                <div className="mb-3 myInput wrap-input">
+                                    <span className="icon"><MdLocationCity/></span>
+                                    <AvField 
+                                    name="city"
+                                    label="City / Town"
+                                    type="text"
+                                    required
+                                    placeholder="Enter city / town"
+                                    />
+                                </div>
+                                <div className="mb-3 myInput wrap-input">
+                                    <span className="icon"><MdOutlinePlace /></span>
+                                    <AvField 
+                                    name="Postalcode"
+                                    label="Postal Code"
+                                    type="number"
+                                    required
+                                    placeholder="Enter Postal Code"
+                                    />
+                                </div>
+                            
+                            </Col>
+                            <Col lg>
+                                
+                                <div className="mb-3 myInput wrap-input">
+                                    <span className="icon"><MdOutlinePlace /></span>
+                                    <AvField 
+                                
+                                    name="suburb"
+                                    label="Suburb"
+                                    className="form-control first-form"
+                                    placeholder="Enter suburb"
+                                    type="text"
+                                    required
+                                    />
+                                </div>
+                                <div className="mb-3 myInput wrap-input">
+                                    <span className="icon"><FaStreetView /></span>
+                                    <AvField
+                                    name="street"
+                                    label="Street Address"
+                                    type="text"
+                                    required
+                                    placeholder="Enter street address"
+                                    />
+                                </div>
+                                <div className="mb-3 myInput wrap-input">
+                                    <span className="icon"><RiBuildingLine /></span>
+                                    <AvField
+                                    name="building"
+                                    label="Complex / Building"
+                                    type="text"
+                                    required
+                                    placeholder="Enter complex / building"
+                                    />
+                                </div>
+                                
+                            </Col>
+                           
+                        </Row>
+                        <Row className="align-items-center justify-content-center mb-3 mt-4">
+                            <Col  className="text-end">
+                            <button   type="submit" btnType="my-default" className='btn-update'>
+                                Update
+                            </button>
+                            </Col>
+                        </Row>
+                            
+
+                        
+                        </AvForm>
+            </CardBody>
+      </Card>
     )
 }
 
-export default AddressDetails
+export default PasswordAccount
+
+
+
+
+
+
--- src/pages/User-Account/Profile-info/index.js
+++ src/pages/User-Account/Profile-info/index.js
@@ -3,6 +3,29 @@ import MetaTags from 'react-meta-tags';
 import { Col, Container, Row } from "reactstrap"
 import Breadcrumb from "components/Common/Breadcrumb";
 import PageWrapper from "components/PageWrapper";
+import classnames from "classnames"
+import "./style.scss";
+import { Link } from "react-router-dom"
+import {
+    Card,
+    CardBody,
+    CardHeader,
+    CardSubtitle,
+    CardText,
+    CardTitle,
+    Col,
+    Collapse,
+    Container,
+    Nav,
+    NavItem,
+    NavLink,
+    Row,
+    TabContent,
+    TabPane,
+  } from "reactstrap"
+  import AddressDetails from "./components/AddressDetails"
+  import PasswordAccount from "./components/PasswordAccount"
+  import PersonalDetails from "./components/PersonalDetails"
 
 const Profile = () => {
 
@@ -14,7 +37,72 @@ const Profile = () => {
                 <PageWrapper>
                     <Breadcrumb default="/profile" defaultName="Profile" title="my profile" />
 
-                    <h1 className="page-title">Profile pages</h1>
+                    <Nav tabs justified className="nav-tabs-custom">
+                      <NavItem>
+                        <NavLink
+                          style={{ cursor: "pointer" }}
+                          className={classnames({
+                            active: this.state.customActiveTab === "1",
+                          })}
+                          onClick={() => {
+                            this.toggleCustom("1")
+                          }}
+                        >
+                          <span className="d-none d-sm-block">Personal Details</span>
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          style={{ cursor: "pointer" }}
+                          className={classnames({
+                            active: this.state.customActiveTab === "2",
+                          })}
+                          onClick={() => {
+                            this.toggleCustom("2")
+                          }}
+                        >
+                          <span className="d-none d-sm-block">Address Details</span>
+                        </NavLink>
+                      </NavItem>
+                      <NavItem>
+                        <NavLink
+                          style={{ cursor: "pointer" }}
+                          className={classnames({
+                            active: this.state.customActiveTab === "3",
+                          })}
+                          onClick={() => {
+                            this.toggleCustom("3")
+                          }}
+                        >
+                          <span className="d-none d-sm-block">Password Details</span>
+                        </NavLink>
+                      </NavItem>
+                    </Nav>
+
+                    <TabContent activeTab={this.state.customActiveTab} className="p-3 text-muted">
+                      <TabPane tabId="1">
+                        <Row>
+                          <Col sm="12">
+                            <PersonalDetails />
+                          </Col>
+                        </Row>
+                      </TabPane>
+                      <TabPane tabId="2">
+                        <Row>
+                          <Col sm="12">
+                          <AddressDetails />
+                          </Col>
+                        </Row>
+                      </TabPane>
+                      <TabPane tabId="3">
+                        <Row>
+                          <Col sm="12">
+                          <PasswordAccount />
+                          </Col>
+                        </Row>
+                      </TabPane>
+                    </TabContent>
+
                 </PageWrapper>
 
             </div>
