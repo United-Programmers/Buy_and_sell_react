519f9c71b7a64633064da4a8a0b5e089d53cd4c1
Chadrack kyungu <119260399+JohnDevJs@users.noreply.github.com>
2022-12-26 08:46:55 +0200

    done with the footer

--- src/components/HorizontalLayout/Footer.js
+++ src/components/HorizontalLayout/Footer.js
@@ -1,5 +1,7 @@
 import React from "react"
+import { AiFillInstagram, AiFillLinkedin, AiOutlineGithub } from "react-icons/ai"
 import { BsFacebook } from "react-icons/bs"
+import { FaTwitter } from "react-icons/fa"
 import { Container, Row, Col } from "reactstrap"
 
 const Footer = () => {
@@ -31,45 +33,45 @@ const Footer = () => {
                   
                     <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
               
-                      <h6 className="text-uppercase fw-bold">Products</h6>
+                      <h6 className="text-uppercase fw-bold">Customer Care</h6>
                       <hr
                       className="mb-4 mt-0 d-inline-block mx-auto"
                        // style="width: 60px; background-color: #7c4dff; height: 2px"
                       />
 
                       <p>
-                       <a href="#!" className="text-white">MDBootstrap</a>
+                       <a href="#!" className="text-white">Help center</a>
                       </p>
 
                       <p>
-                        <a href="#!" className="text-white">MDWordPress</a>
+                        <a href="#!" className="text-white">How to buy</a>
                       </p>
 
                       <p>
-                        <a href="#!" className="text-white">BrandFlow</a>
+                        <a href="#!" className="text-white">Track your order</a>
                       </p>
 
                       <p>
-                       <a href="#!" className="text-white">Bootstrap Angular</a>
+                       <a href="#!" className="text-white">Return & refund</a>
                       </p>
                     </div>
                   
                     
                     <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
 
-                          <h6 class="text-uppercase fw-bold">Useful links</h6>
+                          <h6 class="text-uppercase fw-bold">Register As</h6>
                           <hr
                           class="mb-4 mt-0 d-inline-block mx-auto"
                           // style="width: 60px; background-color: #7c4dff; height: 2px"
                           />
                           <p>
-                            <a href="#!" className="text-white">Your Account</a>
+                            <a href="#!" className="text-white">Buyer</a>
                           </p>
                           <p>
-                            <a href="#!" className="text-white">Become an Affiliate</a>
+                            <a href="#!" className="text-white">Seller</a>
                           </p>
                           <p>
-                            <a href="#!" className="text-white">Shipping Rates</a>
+                            <a href="#!" className="text-white">Driver</a>
                           </p>
                           <p>
                             <a href="#!" className="text-white">Help</a>
@@ -78,15 +80,15 @@ const Footer = () => {
                 
                       <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
                 
-                        <h6 className="text-uppercase fw-bold">Contact</h6>
+                        <h6 className="text-uppercase fw-bold">Contact Us</h6>
                         <hr
                         className="mb-4 mt-0 d-inline-block mx-auto"
                         // style="width: 60px; background-color: #7c4dff; height: 2px"
                         />
-                        <p><i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-                        <p><i className="fas fa-envelope mr-3"></i> info@example.com</p>
-                        <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-                        <p><i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
+                        <p><i className="fas fa-home mr-3"></i> South Africa, johannesburg</p>
+                        <p><i className="fas fa-envelope mr-3"></i> unitedprogrammers772@gmail.com</p>
+                        <p><i className="fas fa-phone mr-3"></i> + 27 68 027 5001</p>
+                        <p><i className="fas fa-print mr-3"></i> + 27 68 027 5001</p>
                       </div>  
                 </div>
 
@@ -98,19 +100,19 @@ const Footer = () => {
 
                 <div>
                   <a href="" className="text-white me-4">
-                    <BsFacebook size={30} />
+                    <BsFacebook size={25} />
                   </a>
                   <a href="" className="text-white me-4">
-                    <i class="fab fa-twitter"></i>
+                    <FaTwitter size={25} />
                   </a>
                   <a href="" class="text-white me-4">
-                    <i class="fab fa-instagram"></i>
+                    <AiFillInstagram size={25} />
                   </a>
                   <a href="" className="text-white me-4">
-                    <i class="fab fa-linkedin"></i>
+                    <AiFillLinkedin size={25} />
                   </a>
                   <a href="" className="text-white me-4">
-                    <i class="fab fa-github"></i>
+                    <AiOutlineGithub size={25} />
                   </a>
                 </div>
 
