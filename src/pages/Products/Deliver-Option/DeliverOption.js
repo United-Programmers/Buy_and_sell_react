696f2aa077ee0e7007377852ac1c81ddcf48a97e
Chadrack kyungu <65617666+chadrackkyungu@users.noreply.github.com>
2022-12-27 07:28:29 +0200

    new changes

--- src/pages/Products/Deliver-Option/DeliverOption.js
+++ src/pages/Products/Deliver-Option/DeliverOption.js
@@ -7,7 +7,7 @@ import MetaTag from "components/MetaTag";
 import deliTruck from "./components/img/deliTruck.png";
 import collect from './components/img/collect.png';
 import { AiTwotoneLock } from "react-icons/ai";
-import OrderSummary from "./components/OrderSummary";
+// import OrderSummary from "./components/OrderSummary";
 import { MdLocalGroceryStore } from "react-icons/md";
 
 
@@ -22,53 +22,53 @@ const DeliverOption = () => {
                     <Breadcrumb default="/deliver-option" defaultName="Deliver" title="option" />
 
                     {/* <Card> */}
-                        {/* <CardBody> */}
-                            <div className="container" style={{width:"70pc"}}>
-                                <div >
+                    {/* <CardBody> */}
+                    <div className="container" style={{ width: "70pc" }}>
+                        <div >
                             {/* WRITE YOUR CODE HERE OR IN THE COMPONENTS FOLDER */}
                             <div className="header">
-                           <div className="heading1"> <h3> DELIVERY<span style={{color:"rgb(170, 179, 179)"}}>/ PAYMENT/CONFIRMATION </span></h3></div>
-                            <div className="grocery" style={{color:"rgba(169, 65, 194, 1)"}}><MdLocalGroceryStore/><a>Go to Cart</a></div>
+                                <div className="heading1"> <h3> DELIVERY<span style={{ color: "rgb(170, 179, 179)" }}>/ PAYMENT/CONFIRMATION </span></h3></div>
+                                <div className="grocery" style={{ color: "rgba(169, 65, 194, 1)" }}><MdLocalGroceryStore /><a>Go to Cart</a></div>
                             </div>
                             <div className="contents">
-                            <div className="options">
-                               <div className="deliOption">
-                                <div className="deliOptionContents">
-                                <img className="deliTruck" src={deliTruck}/>
-                                <div className="titleDesc">
-                                <h4>Delivery</h4>
-                                <p>Courier delivery to your door</p>
-                                </div>   
-                                <div className="deliButton">
-                                    <button type="button " class="btn btn-primary"><b>Delivery</b></button>
-                                </div> 
+                                <div className="options">
+                                    <div className="deliOption">
+                                        <div className="deliOptionContents">
+                                            <img className="deliTruck" src={deliTruck} />
+                                            <div className="titleDesc">
+                                                <h4>Delivery</h4>
+                                                <p>Courier delivery to your door</p>
+                                            </div>
+                                            <div className="deliButton">
+                                                <button type="button " class="btn btn-primary"><b>Delivery</b></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="collectOption">
+                                        <div className="collectOptionContents">
+                                            <img src={collect} />
+                                            <div className="titleDescDis">
+                                                <h4>Collect Not Available</h4>
+                                                <p>sorry, some items in cart are not eligible for collection</p>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                               </div>
-                               <div className="collectOption">
-                                <div className="collectOptionContents">
-                                <img src={collect}/>
-                                <div className="titleDescDis">
-                                <h4>Collect Not Available</h4>
-                                <p>sorry, some items in cart are not eligible for collection</p>
+                                <div className="summary">
+                                    <div className='summaryDesc'>
+                                        <h4>Order Summary</h4>
+                                        <p>2 Items &emsp;&emsp;&emsp;&emsp;R15,247</p>
+                                        <hr />
+                                        <p>TO PAY: &emsp;&emsp;&emsp;<b style={{ color: "rgba(169, 65, 194, 1)" }}>R15,247</b></p>
+                                        <hr />
+                                        <p><AiTwotoneLock style={{ marginBottom: "4px" }} /> <b>Secure Checkout</b></p>
+                                    </div>
+
                                 </div>
-                                </div>
-                                </div> 
                             </div>
-                            <div className="summary">
-                            <div className='summaryDesc'>
-                            <h4>Order Summary</h4>
-                            <p>2 Items &emsp;&emsp;&emsp;&emsp;R15,247</p>
-                            <hr/>
-                            <p>TO PAY: &emsp;&emsp;&emsp;<b style={{color:"rgba(169, 65, 194, 1)"}}>R15,247</b></p>
-                        <hr/>
-                        <p><AiTwotoneLock style={{marginBottom:"4px"}}/> <b>Secure Checkout</b></p>
                         </div>
-                            
-                            </div>
-                            </div>
-                            </div>
-                            </div>
-                        {/* </CardBody> */}
+                    </div>
+                    {/* </CardBody> */}
                     {/* </Card> */}
                 </PageWrapper>
 
