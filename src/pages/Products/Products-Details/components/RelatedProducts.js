9b60234e17345a09ed7ccf9c1e9ae9d788fab5be
Chadrack kyungu <65617666+chadrackkyungu@users.noreply.github.com>
2022-12-27 09:07:04 +0200

    Added Related product file and install swiper dependencies

---
+++ src/pages/Products/Products-Details/components/RelatedProducts.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card, CardBody } from 'reactstrap'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+function RelatedProducts() {
+    return (
+        <Card>
+            <CardBody>
+                <h1> Related products </h1>
+                {/* WRITE YOUR CODE HERE */}
+            </CardBody>
+        </Card>
+    )
+}
+
+export default RelatedProducts
