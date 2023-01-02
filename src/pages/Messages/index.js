21efc8302dc6959533e27cfda0b0d763aefa6b47
Chadrack kyungu <65617666+chadrackkyungu@users.noreply.github.com>
2023-01-02 12:22:20 +0200

    new changes on the message

--- src/pages/Messages/index.js
+++ src/pages/Messages/index.js
@@ -3,8 +3,18 @@ import MetaTags from 'react-meta-tags';
 import { Col, Container, Row } from "reactstrap"
 import Breadcrumb from "components/Common/Breadcrumb";
 import PageWrapper from "components/PageWrapper";
+import classnames from "classnames"
+import { CardBody, Nav, NavItem, NavLink, TabContent, TabPane, Card } from "reactstrap"
+import { AiOutlineMessage } from "react-icons/ai";
+import Admin from "./Admin/index"
+import Drivers from "./Drivers/index"
+import Vendors from "./Vendors/index"
+import User from "./User/index"
+
 
 const Notification = () => {
+    const [customActiveTab, setCustomActiveTab] = React.useState("1")
+    const toggleCustom = (tab) => customActiveTab !== tab ? setCustomActiveTab(tab) : null
 
     return (
         <React.Fragment>
@@ -12,9 +22,66 @@ const Notification = () => {
                 <MetaTags title_sco="Notifications page | Buy and sell" />
 
                 <PageWrapper>
-                    <Breadcrumb default="/user-message" defaultName="Message" title="my messages" />
+                    <Breadcrumb default="/messages" defaultName="Message" title="my messages" />
 
-                    <h1 className="page-title">Notification page</h1>
+                    <Card>
+                        <Nav tabs justified className="nav-tabs-custom">
+                            <NavItem>
+                                <NavLink
+                                    style={{ cursor: "pointer" }}
+                                    className={classnames({ active: customActiveTab === "1" })}
+                                    onClick={() => toggleCustom("1")}>
+                                    <span className="d-none d-sm-block"> <span className="text-custom-primary"> <AiOutlineMessage size={24} /> </span>   Message from the seller </span>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    style={{ cursor: "pointer" }}
+                                    className={classnames({ active: customActiveTab === "2" })}
+                                    onClick={() => toggleCustom("2")}>
+                                    <span className="d-none d-sm-block">  <span className="text-custom-primary"> <AiOutlineMessage size={24} /> </span>  Message from the driver </span>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    style={{ cursor: "pointer" }}
+                                    className={classnames({ active: customActiveTab === "3" })}
+                                    onClick={() => toggleCustom("3")}>
+                                    <span className="d-none d-sm-block">  <span className="text-custom-primary"> <AiOutlineMessage size={24} /> </span> Message from the admin </span>
+                                </NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink
+                                    style={{ cursor: "pointer" }}
+                                    className={classnames({ active: customActiveTab === "4" })}
+                                    onClick={() => toggleCustom("4")}>
+                                    <span className="d-none d-sm-block">  <span className="text-custom-primary"> <AiOutlineMessage size={24} /> </span> Message from the user </span>
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                    </Card>
+
+                    <TabContent activeTab={customActiveTab} className="p-3 text-muted">
+                        <TabPane tabId="1">
+                            <Admin />
+                        </TabPane>
+                        <TabPane tabId="2">
+                            <CardBody>
+                                <Drivers />
+                            </CardBody>
+                        </TabPane>
+                        <TabPane tabId="3">
+                            <CardBody>
+                                <Vendors />
+                            </CardBody>
+                        </TabPane>
+                        <TabPane tabId="4">
+                            <CardBody>
+                                <User />
+                            </CardBody>
+                        </TabPane>
+                    </TabContent>
+
                 </PageWrapper>
 
             </div>
