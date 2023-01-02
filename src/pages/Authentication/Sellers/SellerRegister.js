a56a89356c0e2cccdac7d477eddfee5c975acbed
Chadrack kyungu <65617666+chadrackkyungu@users.noreply.github.com>
2023-01-02 08:54:10 +0200

    done with seller registration

--- src/pages/Authentication/Sellers/SellerRegister.js
+++ src/pages/Authentication/Sellers/SellerRegister.js
@@ -1,85 +1,175 @@
 import PropTypes from "prop-types"
+import "./style.scss"
 import React, { useEffect } from "react"
 import MetaTags from 'react-meta-tags';
 import { Row, Col, CardBody, Card, Alert, Container } from "reactstrap"
-import { AvForm, AvField } from "availity-reactstrap-validation"
+import {
+  AvForm,
+  AvField,
+  AvRadioGroup,
+  AvRadio,
+} from "availity-reactstrap-validation"
 import { registerUser, apiError, registerUserFailed } from "../../../store/actions"
 import { connect } from "react-redux"
 import { Link } from "react-router-dom"
 import MetaTagComp from "components/MetaTag";
+import CustomBtn from "components/CustomBtn";
+import { CiLogin } from "react-icons/ci";
 
 
 const SellerRegister = () => {
 
 
-    return (
-        <React.Fragment>
-            <MetaTagComp title_sco="Sellers register page | Buy and sell" />
+  return (
+    <React.Fragment>
+      <MetaTagComp title_sco="Sellers register page | Buy and sell" />
+      <div className="account-pages my-5 pt-sm-5">
+        <Row className="justify-content-center mt-5">
+          <Col md={10}>
+            <Card className="overflow-hidden">
+              <CardBody className="p-4">
+                <h5 className="text-center">Register as a seller</h5>
+                <div className="p-3">
+                  <AvForm
+                    className="mt-2"
+                    onValidSubmit={(e, v) => {
+                      handleValidSubmit(e, v)
+                    }}
+                  >
+                    <Row>
+                      <Col sm={4}>
+                        <div className="mb-3 myInput">
+                          <AvField
+                            id="firstName"
+                            name="firstName"
+                            label="First Name"
+                            className="form-control"
+                            placeholder="Enter First"
+                            type="text"
+                            required
+                          />
+                        </div>
+                        <div className="mb-3 myInput">
+                          <AvField
+                            id="email"
+                            name="email"
+                            label="Email"
+                            className="form-control"
+                            placeholder="Enter email"
+                            type="email"
+                            required
+                          />
+                        </div>
+                        <div className="mb-3 myInput">
+                          <AvField
+                            id="website"
+                            name="website"
+                            label="Website"
+                            className="form-control"
+                            placeholder="Enter Website"
+                            type="text"
+                            required
+                          />
+                        </div>
+                      </Col>
 
-            <div className="account-pages my-5 pt-sm-5">
-                <Container>
-                    <Row className="justify-content-center">
-                        <Col md={8} lg={6} xl={4}>
-                            <Card className="overflow-hidden">
-                                <CardBody className="p-4">
-                                    <div className="p-3">
-                                        <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
+                      <Col sm={4}>
+                        <div className="mb-3 myInput">
+                          <AvField
+                            id="lastName"
+                            name="lastName"
+                            label="Last Name"
+                            className="form-control"
+                            placeholder="Enter Last"
+                            type="text"
+                            required
+                          />
+                        </div>
+                        <div className="mb-3 myInput">
+                          <AvField
+                            id="phoneNumber"
+                            name="phoneNumber"
+                            label="Phone Number"
+                            className="form-control"
+                            placeholder="Enter email"
+                            type="number"
+                            required
+                          />
+                        </div>
+                        <div className="mb-3 myInput">
+                          <AvField
+                            id="social"
+                            name="social"
+                            label="Social"
+                            className="form-control"
+                            placeholder="Enter Social"
+                            type="text"
+                            required
+                          />
+                        </div>
+                      </Col>
 
-                                            <div className="mb-3">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="Email"
-                                                    className="form-control"
-                                                    placeholder="Enter email"
-                                                    type="email"
-                                                    required
-                                                />
-                                            </div>
+                      <Col sm={4} className="radio-flex">
+                        <div className="mb-3 myInput">
+                          <AvField
+                            id="company"
+                            name="company"
+                            label="Company Name"
+                            className="form-control"
+                            placeholder="Enter Company"
+                            type="text"
+                            required
+                          />
+                        </div>
+                        <div className="mb-3 myInput" >
+                          <AvField
+                            id="category"
+                            name="category"
+                            label="Category"
+                            className="form-control"
+                            placeholder="Enter Category"
+                            type="text"
+                            required
+                          />
+                        </div>                       
+                      </Col>
 
-                                            <div className="mb-3 row">
-                                                <div className="col-12 text-end">
-                                                    <button
-                                                        className="btn btn-primary w-md waves-effect waves-light"
-                                                        type="submit"
-                                                    >
-                                                        Register
-                                                    </button>
-                                                </div>
-                                            </div>
-
-                                            <div className="mt-2 mb-0 row">
-                                                <div className="col-12 mt-4">
-                                                    <p className="mb-0">
-                                                        By registering you agree to the Veltrix{" "}
-                                                        <Link to="#" className="text-primary">
-                                                            Terms of Use
-                                                        </Link>
-                                                    </p>
-                                                </div>
-                                            </div>
-                                        </AvForm>
-                                    </div>
-                                </CardBody>
-                            </Card>
-                            <div className="mt-5 text-center">
-                                <p>
-                                    Already have an account ?
-                                    <Link to="/login" className="fw-medium text-primary">
-                                        Login
-                                    </Link>
-                                </p>
-                                <p>
-                                    © {new Date().getFullYear()}  Crafted with
-                                    <i className="mdi mdi-heart text-danger" /> by The united programmers
-                                </p>
-                            </div>
-                        </Col>
+                      <div className="mb-3 mt-3 radioInput">
+                          <AvRadioGroup
+                            inline
+                            name="vat"
+                            label="Are you VAT registered ?"
+                            required
+                          >
+                            <AvRadio label="Yes" value="Yes" />
+                            <AvRadio label="No" value="No" />
+                          </AvRadioGroup>
+                        </div>
                     </Row>
-                </Container>
-            </div>
-        </React.Fragment>
-    )
+
+                    <div className="text-center me-3">
+                      <CustomBtn btnName="Submit" icon={<CiLogin size={28} />} />
+                    </div>
+                  </AvForm>
+                </div>
+
+                <div className="mt-1 text-start">
+                  <p>
+                    Already have an account ?
+                    <Link to="/login" className="fw-medium text-primary">
+                      Login
+                    </Link>
+                  </p>
+                </div>
+
+              </CardBody>
+            </Card>
+
+          </Col>
+        </Row>
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default SellerRegister;
