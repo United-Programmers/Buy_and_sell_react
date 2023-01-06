113d26fd4020b7dc60949a2fd63ed88ddf87c654
kito078 <101482292+kito078@users.noreply.github.com>
2023-01-06 02:34:59 +0200

    few changes

--- src/pages/Products/Home-Products/components/Product2.js
+++ src/pages/Products/Home-Products/components/Product2.js
@@ -6,48 +6,36 @@ import { MdShoppingCart } from 'react-icons/md';
 import { HiInformationCircle } from 'react-icons/hi';
 import {HiCamera } from 'react-icons/hi';
 import { productData } from "./productData";
-
-function Products() {
+import Zoom from "./images/zoom.webp";
+function Products(props) {
     return (
 
-      
-        
-            <Row className="product_container align-items-center mb-4">
-                {
-                    productData.map((data,i)=>{
-                        return(
-                        <Col md={3}>
-                        <div className="product-2  d-flex swipe__item pro__image text-center align-items-center ">
-                            <img src={data.img} alt="" />
-                            <span className="pro__camera"><HiCamera/></span>
-                            <span className="pro__new">
-                              {data.new}
-                            </span>    
-                            <span className="pro__off">
-                              <span>{data.num}</span>
-                              <span>{data.off}</span>
+            <Col md>
+                <div className="product-2  d-flex swipe__item pro__image text-center align-items-center ">
+                    <img src={Zoom} alt="" />
+                    <span className="pro__camera"><HiCamera/></span>
+                    <span className="pro__new">
+                    {props.new}
+                    </span>    
+                    <span className="pro__off">
+                        <span>{props.num}</span>
+                        <span>OFF</span>
+
+                    </span>    
+                        
+                    <Row className="item__content mt-3">
+                        <h6>Cooler Master Compact Home And Business Core i3 Pro Desktop Computer</h6>
+                        <h6 className="label__item">Cooler Master</h6>
+                        <h5 className="pro__price">R 12,999<span className="price__cancel"><del>R 13,999</del> <span className="i-icon"><HiInformationCircle/></span> </span></h5>
+                        <h6 className="pro__stock">In stock <span className="pro__location">JHB</span> {""} <span className="pro__location">PTA</span> </h6>
+                        <span className="star">
+                        <AiTwotoneStar/><span className="star__content">4.7(71)</span>
+                        </span>
+                    </Row>
+                    <div className="btn-grp "><button className="cart__btn view__btn"><span className="icon"><MdShoppingCart/></span>View Product</button></div>
+                </div>
+            </Col>
     
-                            </span>    
-                              
-                            <Row className="item__content mt-3">
-                                <h6>{data.desc}</h6>
-                                <h6 className="label__item">{data.brand}</h6>
-                                <h5 className="pro__price">{data.price} <span className="price__cancel"><del>{data.del}</del> <span className="i-icon"><HiInformationCircle/></span> </span></h5>
-                                <h6 className="pro__stock">{data.stock} <span className="pro__location">{data.location}</span> {""} <span className="pro__location">{data.location2}</span> </h6>
-                                <span className="star">
-                                <AiTwotoneStar/><span className="star__content">{data.star}</span>
-                                </span>
-                            </Row>
-                            <div className="btn-grp "><button className="cart__btn view__btn"><span className="icon"><MdShoppingCart/></span>{data.btn}</button></div>
-                        </div>
-                    </Col>
-                        )
-                    })
-                }
-                
-               
-            </Row>
-        
     )
 }
 
--- src/pages/Products/Home-Products/components/Products2.js
+++ src/pages/Products/Home-Products/components/Products2.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import Product2 from "./Product2"
+import React from 'react';
+import Product2 from "./Product2";
 import { productData } from "./productData";
+import { Button, Col, Container, Row, CardBody, Card } from "reactstrap"
 
 function Products2() {
     const results= productData.map(eachData=>{
         return (
-          <Product2/>
+          <Row xs={3} className='products2 '>
+            <Product2 new={eachData.new} num={eachData.num}/>
+          </Row> 
         )
     })
   return (
    
-    <div className='product2'>
-        
-      {results}
-        
-        
+    <div >
+        <Row md={2} lg={2} xl={3}  className='products2 sm:grid md:-cols-2 xl:grid-cols-3 align-items-center justify-content-center '>
+          {results}
+        </Row>
+      
+   
     </div>
   )
 }
