eb585837c21873192fb659afa8b8f616ced6607e
Hammad Rao <106843117+hammadrao891@users.noreply.github.com>
2023-01-08 18:26:56 +0500

    Update SeetingsForm.js

--- src/pages/Vendors/Profile-account/Account-settings/components/SeetingsForm.js
+++ src/pages/Vendors/Profile-account/Account-settings/components/SeetingsForm.js
@@ -4,9 +4,18 @@ import { AvField } from "availity-reactstrap-validation"
 
 function SeetingsForm() {
   return (
-    <div className="mb-3 myInput">
-    <AvField placeholder="Enter project name" name="projectName" label="Project Name" className="form-control" type="text" required />
+    <>
+     <div style={{display:"flex"}} className='forms'>
+     <div style={{width:"50pc"}} className="m-4 myInput">
+      <AvField placeholder="Enter First Name" name="projectName" label="First Name" className="form-control" type="text" required />
+     <AvField placeholder="Enter Last Name" name="projectName" label="Last Name" className="form-control" type="text" required />
     </div>
+   <div style={{width:"50pc"}} className="m-4 myInput">
+    <AvField placeholder="Enter Email" name="projectName" label="Email" className="form-control" type="text" required />
+     <AvField placeholder="Enter Shop Name" name="projectName" label="Shop Name" className="form-control" type="text" required />
+    </div>
+    </div>
+    </>
   )
 }
 
