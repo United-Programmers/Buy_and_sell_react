588f56adb00b43914d5acead004fc1cb1a478dda
kito078 <101482292+kito078@users.noreply.github.com>
2023-01-09 03:30:54 +0200

    vendor update

--- src/pages/Vendors/Profile-account/Dashboard/components/Cards.js
+++ src/pages/Vendors/Profile-account/Dashboard/components/Cards.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React from 'react';
+import { RxPerson } from 'react-icons/rx';
+import {ImArrowDownLeft2 } from 'react-icons/im';
+import {Row, Card, Col} from "react-bootstrap";
+
 
 function Cards() {
     return (
-        <div>Cards</div>
+        <div className='cards'>
+            <Row>
+                <Col md>
+                    <Card>
+                        <div className='cards__container  '>
+                            <div className="cards__account text-center ">
+                                <RxPerson/>
+                            </div>
+                            <div className='mt-2 ms-2'>
+                                <h6 className='cards__earning'>Earnings before taxes</h6>
+                                <span>
+                                    <span className='cards__price'>R 30 400</span><span className='cards__tax'>+2.5%</span>
+                                </span>
+                            </div>
+                            <div className="cards__icome">
+                              <ImArrowDownLeft2/>
+                            </div>
+                        </div>
+                    </Card>
+                </Col>
+                <Col md>
+                    <Card >
+                        <div className='d-flex align-items-center ms-3'>
+                            <div className="cards__account text-center ">
+                                <RxPerson/>
+                            </div>
+                            <div className='mt-3 ms-2'>
+                                <h6>Ernings before taxes</h6>
+                                <span>
+                                    <span>$3045400.00</span><span>+2.5%</span>
+                                </span>
+                            </div>
+                        </div>
+                    </Card>
+                </Col>
+                <Col md>
+                    <Card >
+                        <div className='d-flex align-items-center ms-3'>
+                            <div className="cards__account text-center ">
+                                <RxPerson/>
+                            </div>
+                            <div className='mt-3 ms-2'>
+                                <h6>Ernings before taxes</h6>
+                                <span>
+                                    <span>$3045400.00</span><span>+2.5%</span>
+                                </span>
+                            </div>
+                        </div>
+                    </Card>
+                </Col>
+            </Row>
+            
+        </div>
     )
 }
 
--- src/pages/Vendors/Profile-account/Dashboard/index.js
+++ src/pages/Vendors/Profile-account/Dashboard/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import "./style.scss"
 import Layout from "../index"
+import Cards from "./components/Cards"
 
 const VendorDashboard = () => {
 
     return (
         <Layout>
             <h1 className="page-title"> Vendor Dashboard </h1>
+            <Cards/>
         </Layout>
     )
 }
