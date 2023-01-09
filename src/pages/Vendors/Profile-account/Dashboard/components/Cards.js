930ec6e4acaf678e7fc466c4c88ab6927151bc1a
kito078 <101482292+kito078@users.noreply.github.com>
2023-01-09 16:55:45 +0200

    card done

--- src/pages/Vendors/Profile-account/Dashboard/components/Cards.js
+++ src/pages/Vendors/Profile-account/Dashboard/components/Cards.js
@@ -1,73 +1,56 @@
 import React from 'react';
 import { RxPerson } from 'react-icons/rx';
 import {ImArrowDownLeft2 } from 'react-icons/im';
+import {BsArrowCounterclockwise } from 'react-icons/bs';
+import {HiOutlineArrowNarrowLeft } from 'react-icons/hi';
 import {Row, Card, Col} from "react-bootstrap";
-
+import { dataCards } from './dataCards';
 
 function Cards() {
     return (
         <div className='cards'>
-            <Row className='align-items-center justify-content-center'>
-                <Col md>
-                    <Card className='card__wrapper'>
+           <div className='cards__container-one'>
+             {dataCards.map(data=>{
+                return(
+                 <div>
+                   <Card className='card__wrapper'>
                         <div className='cards__container  '>
                             <div className="cards__account text-center ">
                                 <RxPerson/>
                             </div>
                             <div className='mt-2 ms-2'>
-                                <h6 className='cards__earning'>Earnings before taxes</h6>
+                                
+                                
+                                <h6 className='cards__earning'>{data.earn}</h6>
                                 <span>
-                                    <span className='cards__price'>R 30 400</span><span className='cards__tax'>+2.5%</span>
+                                    <span className='cards__price'>{data.amount}</span><span className='cards__tax'>{data.tax}</span>
+                                   
                                 </span>
+                                <div className="cards__month">
+                                      <span>last Month <HiOutlineArrowNarrowLeft/></span>
+                                </div>
+                              
+                                
                             </div>
                             <div className="cards__icome">
                               <ImArrowDownLeft2/>
                             </div>
                         </div>
                     </Card>
-                </Col>
-                <Col md>
-                    <Card className='card__wrapper'>
-                        <div className='cards__container  '>
-                            <div className="cards__account text-center ">
-                                <RxPerson/>
-                            </div>
-                            <div className='mt-2 ms-2'>
-                                <h6 className='cards__earning'>Earnings before taxes</h6>
-                                <span>
-                                    <span className='cards__price'>R 30 400</span><span className='cards__tax'>+2.5%</span>
-                                </span>
-                            </div>
-                            <div className="cards__icome">
-                              <ImArrowDownLeft2/>
-                            </div>
-                        </div>
-                    </Card>
-                </Col>
-                <Col md>
-                    <Card className='card__wrapper'>
-                        <div className='cards__container  '>
-                            <div className="cards__account text-center ">
-                                <RxPerson/>
-                            </div>
-                            <div className='mt-2 ms-2'>
-                                <h6 className='cards__earning'>Earnings before taxes</h6>
-                                <span>
-                                    <span className='cards__price'>R 30 400</span><span className='cards__tax'>+2.5%</span>
-                                </span>
-                            </div>
-                            <div className="cards__icome">
-                              <ImArrowDownLeft2/>
-                            </div>
-                        </div>
-                    </Card>
-                </Col>
+                </div>
                 
+                )
+             })}
                 
-            </Row>
+               
+                
+               
+           </div>
             
         </div>
     )
 }
 
 export default Cards
+
+
---
+++ src/pages/Vendors/Profile-account/Dashboard/components/dataCards.js
@@ -0,0 +1,17 @@
+export const dataCards= [
+    {
+        earn:"Earnings before taxes",
+        amount:"R 30 400",
+        tax:"+2.3%"
+    },
+    {
+        earn:"Earnings after taxes",
+        amount:"R 20 400",
+        tax:"+2.4%"
+    },
+    {
+        earn:"Pending",
+        amount:"R 10 400",
+        tax:"+2.6%"
+    }
+]
