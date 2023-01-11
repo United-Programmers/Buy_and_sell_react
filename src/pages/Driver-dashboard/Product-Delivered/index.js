8d8ba4fc50260eb1b625dde9e23a226d245bb413
jed-code <jedkazadi@gmail.com>
2023-01-11 17:16:37 +0200

    driver product page

--- src/pages/Driver-dashboard/Product-Delivered/index.js
+++ src/pages/Driver-dashboard/Product-Delivered/index.js
@@ -1,19 +1,17 @@
 import React from "react"
 import "./style.scss"
-import MetaTags from 'react-meta-tags';
+import MetaTags from "react-meta-tags"
 import { Col, Container, Row } from "reactstrap"
-import Breadcrumb from "components/Common/Breadcrumb";
-import PageWrapper from "components/PageWrapper";
+import Breadcrumb from "components/Common/Breadcrumb"
+import PageWrapper from "components/PageWrapper"
 import Layout from "../index"
-
+import ProductTable from "./Components/ProductTable"
 const ProductsToDeliver = () => {
-
-    return (
-        <Layout>
-        {/* Import your components here */}
-        <h2> Product to deliver </h2>
-        </Layout>
-    )
+  return (
+    <Layout>
+      <ProductTable />
+    </Layout>
+  )
 }
 
 export default ProductsToDeliver
