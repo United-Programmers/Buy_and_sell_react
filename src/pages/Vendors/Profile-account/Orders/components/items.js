ac12a32c08ca7919a2bf3d7300850c98c9444f4e
kito078 <101482292+kito078@users.noreply.github.com>
2023-01-11 20:09:58 +0200

    order list update

---
+++ src/pages/Vendors/Profile-account/Orders/components/items.js
@@ -0,0 +1,92 @@
+import Profile from "./images/profile.jpg"
+
+const profileTable={
+    item:[
+        
+        [
+            Profile,
+            "Diai Rios",
+            "#565",
+            "23/2/2023",
+            "Bag",
+            "Paid",
+            "R 4567"
+        ],
+        [
+            Profile,
+            "Diai Rios",
+            "#565",
+            "23/2/2023",
+            "Bag",
+            "Canceled",
+            "R 4567"
+        ],
+        [
+            Profile,
+            "Diai Rios",
+            "#565",
+            "23/2/2023",
+            "Bag",
+            "Paid",
+            "R 4567"
+        ],
+        [
+            Profile,
+            "Diai Rios",
+            "#565",
+            "23/2/2023",
+            "Bag",
+            "Refunded",
+            "R 4567"
+        ],
+        [
+            Profile,
+            "Diai Rios",
+            "#565",
+            "23/2/2023",
+            "Bag",
+            "Paid",
+            "R 4567"
+        ],
+        [
+            Profile,
+            "Diai Rios",
+            "#565",
+            "23/2/2023",
+            "Bag",
+            "Paid",
+            "R 4567"
+        ],
+        [
+            Profile,
+            "Diai Rios",
+            "#565",
+            "23/2/2023",
+            "Bag",
+            "Paid",
+            "R 4567"
+        ],
+    ],
+    columns:[
+        "Profile",
+        "Customer Name",
+        "ID Number",
+        "Date",
+        "Product",
+        "Status",
+        "Revenue",
+    ],
+};
+
+const matem ={
+    books:10,
+    name: "kito",
+    data: ['book','cow'],
+    columns:['bus','lambo']
+}
+
+const data ={
+    profileTable,
+}
+
+export default data;
