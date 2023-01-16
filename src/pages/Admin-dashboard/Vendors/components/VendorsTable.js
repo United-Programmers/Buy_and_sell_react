67c6a329a1221bc3c38226d4e911adf714071c1f
Oluwatosin Abigail Ogundeji <oluwatosinhephzibah@gmail.com>
2023-01-16 10:43:00 +0100

    made some updates on the action buttons and its styles

--- src/pages/Admin-dashboard/Vendors/components/VendorsTable.js
+++ src/pages/Admin-dashboard/Vendors/components/VendorsTable.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState, useRef } from "react";
 import { Table, Pagination } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import data from "../../components/data";
+import data from "../components/data";
 import { Badge, Card } from 'reactstrap';
 import { AiOutlineEye } from "react-icons/ai";
 import { RiDeleteBin6Line } from "react-icons/ri";
@@ -38,7 +38,7 @@ const VendorsTables = () => {
     return (
         <div className="col-12">
             <Card className="p-4">
-                <h4 className="table-card-title"> Buyers </h4>
+                <h4 className="table-card-title"> Vendors </h4>
                 <div>
                     <Table responsive className="w-100">
                         <div id="example_wrapper" className="dataTables_wrapper">
@@ -57,7 +57,7 @@ const VendorsTables = () => {
                                                         {i === 0 ? (
                                                             <img
                                                                 className="rounded-circle avatar-sm"
-                                                                width="35"
+                                                                width="15"
                                                                 src={da}
                                                                 alt=""
                                                             />
@@ -65,9 +65,9 @@ const VendorsTables = () => {
                                                             <Fragment>
                                                                 <div >
                                                                     {da}
-                                                                    {i === 6 && (
+                                                                    {i === 7 && (
                                                                         <div className="d-flex">
-                                                                            <Badge to="#" className="rounded-circle table__btn shadow btn-xs sharp me-1 btn__view p-2">
+                                                                            <Badge to="#" className="rounded-circle table__btn shadow btn-xs sharp me-1 btn__success p-2">
                                                                                 <AiOutlineEye size={14} />
                                                                             </Badge>
                                                                             <Badge to="#" className="rounded-circle btn-danger table__btn shadow btn-xs sharp p-2"
@@ -92,7 +92,7 @@ const VendorsTables = () => {
                                 <div className="dataTables_info">
                                     Showing {activePag.current * sort + 1} to {data.profileTable.data.length < (activePag.current + 1) * sort ? data.profileTable.data.length
                                         : (activePag.current + 1) * sort}
-                                    of {data.profileTable.data.length} entries
+                                     of {data.profileTable.data.length} entries
                                 </div>
 
                                 <div className="dataTables_paginate paging_simple_numbers">
