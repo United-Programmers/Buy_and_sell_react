2ae8bd01f6f39b642c5fe356b1afd3be692efdcc
enemrocky <craigcharltonlunga1000@gmail.com>
2023-01-18 22:14:04 +0200

    created the driver dashboard chart

--- src/pages/Driver-dashboard/Dashboard/index.js
+++ src/pages/Driver-dashboard/Dashboard/index.js
@@ -1,17 +1,24 @@
 import React from "react"
 import "./style.scss"
 import Layout from "../index"
-import { MdOutlineDashboardCustomize } from "react-icons/md";
-import {Row, Card, Col} from "react-bootstrap";
+import { MdOutlineDashboardCustomize } from "react-icons/md"
+import { Row, Card, Col } from "react-bootstrap"
+import DriverChart from "./Components/Charts"
+import Cards from "./Components/Cards"
 
 const DriverDashboard = () => {
-
-    return (
-        <Layout>
-            {/* Your components here */}
-            <h1>  Dashboard </h1>
-        </Layout>
-    )
+  return (
+    <Layout>
+      {/* Your components here */}
+      <h1 className="page-title mt-3 ms-4 mb-4 mt-lg-4  ms-lg-1">
+        <span>
+          <MdOutlineDashboardCustomize />
+        </span>{" "}
+        Driver Dashboard{" "}
+      </h1>
+      <DriverChart />
+    </Layout>
+  )
 }
 
 export default DriverDashboard
