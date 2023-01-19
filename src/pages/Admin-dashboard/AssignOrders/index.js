c3689ab9df616258bf33a0fecabf4b70c8504ebf
sodiq <abdulazeezsodiq403@gmail.com>
2023-01-18 12:09:30 -1200

    Done with Assign Order table task...

--- src/pages/Admin-dashboard/AssignOrders/index.js
+++ src/pages/Admin-dashboard/AssignOrders/index.js
@@ -1,15 +1,16 @@
 import React from "react"
-import "./style.scss"
+// import "./style.scss"
 import Layout from "../index"
 import { MdOutlineDashboardCustomize } from "react-icons/md";
 import { Row, Card, Col } from "react-bootstrap";
+import { AssigOrderPage } from "./components/AssignOrder";
 
 const AssignOrders = () => {
 
     return (
         <Layout>
             {/* Your components here */}
-            <h1> Assign Orders </h1>
+            <AssigOrderPage/>
         </Layout>
     )
 }
