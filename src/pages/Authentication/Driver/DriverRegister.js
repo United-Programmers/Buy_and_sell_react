5cbd0f92b473bf72f969c8fbe1401f5bae22cc6a
Chadrack kyungu <65617666+chadrackkyungu@users.noreply.github.com>
2023-02-07 23:58:06 +0200

    added content inside inputForm js

--- src/pages/Authentication/Driver/DriverRegister.js
+++ src/pages/Authentication/Driver/DriverRegister.js
@@ -11,238 +11,75 @@ import CustomBtn from "components/CustomBtn"
 import { CiLogin } from "react-icons/ci"
 import "./style.scss";
 import { BsFillPersonFill } from "react-icons/bs";
+import InputForm from "./components/InputForm";
+import usePost from "../../../hooks/usePost";
 
 
 const DriverRegister = () => {
+
+    const { execute, pending, data } = usePost()
+
+    const handleValidSubmit = (e, values) => {
+        e.preventDefault();
+        const Method = 'POST', endPoint = 'auth/river/signUp', isJSON = false, token = true;
+
+        const userDate = JSON.stringify({
+            "firstName": values.firstName,
+            "lastName": values.lastName,
+            "phoneNumber": values.phoneNumber,
+            "agreed": values.true[0],
+            "email": values.email,
+            "workRegion": values.workRegion,
+            "passportNumber": values.passportNumber,
+            "licenseDisk": values.licenseDisk,
+            "driverLicense": values.driverLicense,
+            "proofDoc": values.proofDoc,
+            "password": values.Password,
+            "passwordConfirm": values.confirmPassword,
+        });
+
+        execute(endPoint, userDate, Method, RegisterMsg, token, isJSON)
+    }
+
 
 
     return (
         <React.Fragment>
             <MetaTagComp title_sco="Driver register page | Buy and sell" />
 
-            <div style={{marginRight:"160pc"}} className=" account-pages my-5 pt-sm-5 ">
-                    <Row className="justify-content-center mt-5 ">
-                        <Col md={8} lg={6} xl={4}>
-                            <Card className=" overflow-hidden con">
-                                <CardBody className=" ">
-                                {/* <h5 className="text-center">Register as an admin</h5> */}
-                                    <div className="p-3">
-                                        <div className="three">
-                                         <div style={{marginTop:"30px"}} className="first">
-                                            <div className="inputs">
-                                            
-                                            <div>
-                                            <h5>Do you have a vehicle to Drive?</h5>
-                                            
-                                             <input type="radio" value="Yes" name="gender" /> Yes
-                                             <input style={{marginLeft:"10pc"}} type="radio" value="No" name="gender" /> No
-                                             </div>
-                                             <div>
-                                             <h5>Android phone with version 7+?</h5>
-                                             <input type="radio" value="Yes" name="s" /> Yes
-                                             
-                                             <input type="radio" value="No"style={{marginLeft:"10pc"}} name="s" /> No
-                                             </div>
-                                             <div>
-                                             <h5>Consent to a criminal record check?</h5>
-                                             <input type="radio" value="Yes" name="q" /> Yes
-                                             <input type="radio" style={{marginLeft:"10pc"}} value="No" name="q" /> No
-                                             </div>
-                                             <div>
-                                             <h5>Agreed to the terms and conditions?</h5>
-                                             <input type="radio" value="Yes" name="w" /> Yes
-                                             <input type="radio" value="No" style={{marginLeft:"10pc"}} name="w" /> No
-                                             </div>
-                                             <div>
-                                             <h5>Agreed to the driver privacy policy?</h5>
-                                             <input type="radio" value="Yes" name="e" /> Yes
-                                             <input type="radio" style={{marginLeft:"10pc"}} value="No" name="e" /> No
-                                            </div>
-                                            </div>
-                                            </div>  
-                                                 
-                                        <div className="second">
-                                      <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
+            <div className="page-content account-pages my-5 pt-sm-5">
+                <Row className="justify-content-center mt-5 ">
+                    <Col md={12} lg={6} xl={10}>
+                        <Card className="overflow-hidden">
+                            <CardBody>
+                                <h3 className="text-center">Register as a driver</h3>
 
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="First Name"
-                                                    className="form-control"
-                                                    placeholder="Enter First Name"
-                                                    type="text"
-                                                    required
-                                                />
-                                            </div>
+                                <div className="p-3">
+                                    <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
 
-                                        </AvForm>
-                                                                              <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
+                                        <InputForm />
 
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="Last name"
-                                                    className="form-control"
-                                                    placeholder="Enter Last name"
-                                                    type="text"
-                                                    required
-                                                />
-                                            </div>
+                                    </AvForm>
+                                </div>
 
-                                        </AvForm>
+                                <div className="text-center">
+                                    <CustomBtn btnName="Submit" icon={<CiLogin size={28} />} Pending={pending} />
+                                </div>
 
-                                      <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
-
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="Email"
-                                                    className="form-control"
-                                                    placeholder="Enter Email"
-                                                    type="email"
-                                                    required
-                                                />
-                                            </div>
-
-                                        </AvForm>
-
-                                      <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
-
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="Mobile number"
-                                                    className="form-control"
-                                                    placeholder="Enter Mobile number"
-                                                    type="text"
-                                                    required
-                                                />
-                                            </div>
-
-                                        </AvForm>
-
-                                      <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
-
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="Choose work region"
-                                                    className="form-control"
-                                                    placeholder="Enter Choose work region"
-                                                    type="text"
-                                                    required
-                                                />
-                                            </div>
-
-                                        </AvForm>
-
-
-                                        </div>
-
-                                        <div className="third">
-                                      <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
-
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="ID number / Passport"
-                                                    className="form-control"
-                                                    placeholder="Enter ID number / Passport"
-                                                    type="text"
-                                                    required
-                                                />
-                                            </div>
-
-                                        </AvForm>
-                                   
-                                      <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
-
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="License disk  picture"
-                                                    className="form-control"
-                                                    placeholder="Enter License disk  picture"
-                                                    type="email"
-                                                    required
-                                                />
-                                            </div>
-
-                                        </AvForm>
-                                      <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
-
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="Driver license picture"
-                                                    className="form-control"
-                                                    placeholder="Enter Driver license picture"
-                                                    type="text"
-                                                    required
-                                                />
-                                            </div>
-
-                                        </AvForm>
-                                      <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
-
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="Proof of address document"
-                                                    className="form-control"
-                                                    placeholder="Enter Proof of address document"
-                                                    type="text"
-                                                    required
-                                                />
-                                            </div>
-
-                                        </AvForm>
-
-                                         <AvForm className="mt-4" onValidSubmit={(e, v) => { handleValidSubmit(e, v) }}>
-
-                                            <div className="mb-3 myInput">
-                                                <AvField
-                                                    id="email"
-                                                    name="email"
-                                                    label="Upload profile image"
-                                                    className="form-control"
-                                                    placeholder="Choose"
-                                                    type="file"
-                                                    required
-                                                />
-                                            </div>
-
-                                        </AvForm>
-
-                                        </div>
-                                        </div>
-                                          <div style={{marginLeft:"50pc"}} className="but" >
-                                                <CustomBtn style={{marginRight:"30pc"}} variant="primary" size="lg" btnName="Create Account"  />
-                                            </div>
-                                        
-                            <div style={{marginRight:"30pc"}} className="mt-5 text-center">
-                                <p>
-                                    Already have an account ?
-                                    <Link to="/login" className="fw-medium text-primary">
-                                        Login
-                                    </Link>
-                                </p>
-                            </div>
-                                    </div>
-                                </CardBody>
-                            </Card>
-                        </Col>
-                    </Row>
+                                <div className="text-center mt-4">
+                                    <p>
+                                        Already have an account ?
+                                        <Link to="/login" className="fw-medium text-primary">
+                                            Login
+                                        </Link>
+                                    </p>
+                                </div>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
             </div>
+
         </React.Fragment>
     )
 }
---
+++ src/pages/Authentication/Driver/components/InputForm.js
@@ -0,0 +1,189 @@
+import React from 'react'
+import { Row, Col } from "reactstrap"
+import { AvField, AvRadioGroup, AvRadio, AvCheckboxGroup, AvCheckbox, } from "availity-reactstrap-validation"
+
+function InputForm() {
+    return (
+        <div>
+
+            <Row>
+                <Col md={4}>
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="firstName"
+                            label="First Name"
+                            className="form-control"
+                            placeholder="Enter First Name"
+                            type="text"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="lastName"
+                            label="Last name"
+                            className="form-control"
+                            placeholder="Enter Last name"
+                            type="text"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="email"
+                            label="Email"
+                            className="form-control"
+                            placeholder="Enter Email"
+                            type="email"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="password"
+                            label="Password"
+                            className="form-control"
+                            placeholder="Enter your password"
+                            type="password"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="confirmPassword"
+                            label="Confirm Password"
+                            className="form-control"
+                            placeholder="Confirm Password"
+                            type="password"
+                            required
+                        />
+                    </div>
+
+
+                </Col>
+
+                <Col md={4}>
+
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="phoneNumber"
+                            label="Mobile number"
+                            className="form-control"
+                            placeholder="Enter Mobile number"
+                            type="number"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="workRegion"
+                            label="Choose work region"
+                            className="form-control"
+                            placeholder="Enter Choose work region"
+                            type="text"
+                            required
+                        />
+                    </div>
+
+
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="passportNumber"
+                            label="ID Number / Passport"
+                            className="form-control"
+                            placeholder="Enter ID number / Passport"
+                            type="text"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="licenseDisk"
+                            label="License disk  picture"
+                            className="form-control"
+                            placeholder="Enter License disk  picture"
+                            type="file"
+                            required
+                        />
+                    </div>
+
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="driverLicense"
+                            label="Driver license picture"
+                            className="form-control"
+                            placeholder="Enter Driver license picture"
+                            type="file"
+                            required
+                        />
+                    </div>
+
+                </Col>
+
+
+                <Col md={4}>
+                    <div className="mb-3 myInput">
+                        <AvField
+                            name="proofDoc"
+                            label="Proof of address document"
+                            className="form-control"
+                            placeholder="Enter Proof of address document"
+                            type="file"
+                            required
+                        />
+                    </div>
+
+                    {/* <div className="mb-3 myInput">
+                        <AvField
+                            name="profile"
+                            label="Upload profile image"
+                            className="form-control"
+                            placeholder="Choose"
+                            type="file"
+                            required
+                        />
+                    </div> */}
+
+
+                    <div className="mb-3 ">
+                        <label >Android phone with version 7+?</label>
+                        <AvRadioGroup inline name="radioExample2" required>
+                            <AvRadio label="Yes" value="true" />
+                            <AvRadio label="No" value="false" />
+                        </AvRadioGroup>
+                    </div>
+                    <div className="mb-3 ">
+                        <label> Consent to a criminal record check? </label>
+                        <AvRadioGroup inline name="radioExample2" required>
+                            <AvRadio label="Yes" value="true" />
+                            <AvRadio label="No" value="false" />
+                        </AvRadioGroup>
+                    </div>
+                    <div className="mb-3 ">
+                        <label> Agreed to the terms and conditions? </label>
+                        <AvRadioGroup inline name="radioExample2" required>
+                            <AvRadio label="Yes" value="true" />
+                        </AvRadioGroup>
+                    </div>
+                    <div className="mb-3 ">
+                        <label> Agreed to the driver privacy policy? </label>
+                        <AvRadioGroup inline name="radioExample2" required>
+                            <AvRadio label="Yes" value="true" />
+                        </AvRadioGroup>
+                    </div>
+                </Col>
+
+            </Row>
+
+
+        </div>
+    )
+}
+
+export default InputForm
